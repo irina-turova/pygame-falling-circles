9fb52c2c70439cceeaba9b27a13bf8392196171a
Irina Turova <turovaia@yandex.ru>
2019-08-12 21:42:51 +0500

    Fix circles speed

--- falling_balls.py
+++ falling_balls.py
@@ -5,15 +5,17 @@ size = width, height = 400, 300
 screen = pygame.display.set_mode(size)
 baked_screen = pygame.Surface(screen.get_size())
 
+clock = pygame.time.Clock()
 
 circle_color = (0, 0, 255)
 circle_radius = 10
+pixels_per_second_speed = 100
 active_circles_positions = set()
 
 
 def draw_active_circles():
     for pos in active_circles_positions:
-        pygame.draw.circle(screen, circle_color, pos, circle_radius)
+        pygame.draw.circle(screen, circle_color, tuple(map(int, pos)), circle_radius)
 
 
 def process_circles_positions():
@@ -22,11 +24,12 @@ def process_circles_positions():
         return pos[1] >= height - circle_radius
 
     global active_circles_positions
-    active_circles_positions = set(map(lambda p: (p[0], p[1] + 1), active_circles_positions))
+    seconds_tick = clock.tick() / 1000
+    active_circles_positions = set(map(lambda p: (p[0], p[1] + pixels_per_second_speed * seconds_tick), active_circles_positions))
 
     for pos in active_circles_positions:
         if is_fallen(pos):
-            pygame.draw.circle(baked_screen, circle_color, pos, circle_radius)
+            pygame.draw.circle(baked_screen, circle_color, tuple(map(int, pos)), circle_radius)
 
     active_circles_positions = set(filter(lambda p: not is_fallen(p), active_circles_positions))
 
