f5b80a9af09e2481cfdd91886f7b3c951d875152
Irina Turova <turovaia@yandex.ru>
2019-08-12 21:27:20 +0500

    Fix fallen circles position

--- falling_balls.py
+++ falling_balls.py
@@ -7,23 +7,28 @@ baked_screen = pygame.Surface(screen.get_size())
 
 
 circle_color = (0, 0, 255)
+circle_radius = 10
 active_circles_positions = set()
 
 
 def draw_active_circles():
     for pos in active_circles_positions:
-        pygame.draw.circle(screen, circle_color, pos, 10)
+        pygame.draw.circle(screen, circle_color, pos, circle_radius)
 
 
 def process_circles_positions():
+
+    def is_fallen(pos):
+        return pos[1] >= height - circle_radius
+
     global active_circles_positions
     active_circles_positions = set(map(lambda p: (p[0], p[1] + 1), active_circles_positions))
 
     for pos in active_circles_positions:
-        if pos[1] >= height:
-            pygame.draw.circle(baked_screen, circle_color, pos, 10)
+        if is_fallen(pos):
+            pygame.draw.circle(baked_screen, circle_color, pos, circle_radius)
 
-    active_circles_positions = set(filter(lambda p: p[1] < height, active_circles_positions))
+    active_circles_positions = set(filter(lambda p: not is_fallen(p), active_circles_positions))
 
 
 is_running = True
