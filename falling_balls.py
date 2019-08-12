be98d2052d8d3dad9ea0f7fdacfbd611b9e1e73a
Irina Turova <turovaia@yandex.ru>
2019-08-12 20:19:46 +0500

    Draw static circles on mouse click

--- falling_balls.py
+++ falling_balls.py
@@ -4,6 +4,14 @@ pygame.init()
 size = width, height = 400, 300
 screen = pygame.display.set_mode(size)
 
+active_circles_positions = set()
+
+
+def draw_active_circles():
+    for pos in active_circles_positions:
+        pygame.draw.circle(screen, (0, 0, 255), pos, 10)
+
+
 is_running = True
 while is_running:
     screen.fill((0, 0, 0))
@@ -11,6 +19,11 @@ while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("button pressed", event.pos)
+            active_circles_positions.add(event.pos)
+
+    draw_active_circles()
 
     pygame.display.flip()
 
