6bc6235029a6d48c604ddb165a3a8e671a7e11cc
Irina Turova <turovaia@yandex.ru>
2019-08-12 19:47:46 +0500

    Add basic main loop

--- falling_balls.py
+++ falling_balls.py
@@ -3,3 +3,15 @@ import pygame
 pygame.init()
 size = width, height = 400, 300
 screen = pygame.display.set_mode(size)
+
+is_running = True
+while is_running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+    pygame.display.flip()
+
+pygame.quit()
